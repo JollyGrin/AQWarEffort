cfbaf49c99643b5dc9f882f17a4a84e286076621
JollyGrin <jollygrin@protonmail.com>
2020-07-26 03:00:17 +0200

    update

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3adf4bd21ff5a6b97747";
+/******/ 	var hotCurrentHash = "c747801f12feba22a48c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
